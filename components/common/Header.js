2044f8b3d67998ac7daa96c44b5bf33d55b6aca3
subhasis020299 <subhasisdas125@gmail.com>
2021-11-24 21:52:15 +0530

    navbar ui design barebone

--- components/common/Header.js
+++ components/common/Header.js
@@ -1,7 +1,67 @@
 import React from "react";
+import {
+	Navbar,
+	Nav,
+	Container,
+	Offcanvas,
+	Form,
+	FormControl,
+	Button,
+} from "react-bootstrap";
+
+import styles from "../../styles/Header.module.css";
 
 const Header = () => {
-	return <div>Header</div>;
+	return (
+		<Navbar variant="dark" expand={false} bg="primary">
+			<Container fluid>
+				<Navbar.Brand href="/">PMDb Next!</Navbar.Brand>
+				<Navbar.Toggle
+					aria-controls="offcanvasNavbar"
+					className="remove-focus"
+				/>
+				<Navbar.Offcanvas
+					id="offcanvasNavbar"
+					aria-labelledby="offcanvasNavbarLabel"
+					placement="end"
+					className={styles.header_offcanvas}
+				>
+					<Offcanvas.Header closeButton closeVariant="white">
+						<Offcanvas.Title id="offcanvasNavbarLabel">
+							PMDb Next!
+						</Offcanvas.Title>
+					</Offcanvas.Header>
+					<Offcanvas.Body>
+						<Form className="d-flex remove-focus">
+							<FormControl
+								type="search"
+								placeholder="Search for a movie..."
+								className="me-2 remove-focus"
+								aria-label="Search"
+							/>
+							<Button variant="outline-light remove-focus">Search</Button>
+						</Form>
+						<hr />
+						<Nav className="justify-content-end flex-grow-1 pe-3">
+							<Nav.Link href="/explore" className={styles.header_link}>
+								Explore
+							</Nav.Link>
+							<Nav.Link href="/auth" className={styles.header_link}>
+								Login
+							</Nav.Link>
+							<Nav.Link href="/profile" className={styles.header_link}>
+								{" "}
+								Profile
+							</Nav.Link>
+							<Nav.Link href="/watchlist" className={styles.header_link}>
+								My Watch List
+							</Nav.Link>
+						</Nav>
+					</Offcanvas.Body>
+				</Navbar.Offcanvas>
+			</Container>
+		</Navbar>
+	);
 };
 
 export default Header;
