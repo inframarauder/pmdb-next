13d6b3165ab5c9b80108be6be19c1b5605b1193e
subhasis020299 <subhasisdas125@gmail.com>
2021-11-27 03:39:23 +0530

    added auth context

---
+++ contexts/AuthContext.js
@@ -0,0 +1,51 @@
+import createDataContext from "./createDataContext";
+import axios from "axios";
+
+const initialState = {
+	user: null,
+	loading: false,
+	error: "",
+};
+
+const authReducer = (state, action) => {
+	const { type, payload } = action;
+	switch (type) {
+		case "auth_success":
+			return { ...state, user: payload, loading: false };
+		case "auth_loading":
+			return { ...state, loading: true };
+		case "auth_error":
+			return { ...state, error: payload, loading: false };
+		case "logout":
+			return { ...state, user: null };
+		default:
+			return state;
+	}
+};
+
+const authenticate =
+	(dispatch) =>
+	async ({ username, password }) => {
+		dispatch({ type: "auth_loading" });
+		try {
+			const response = await axios.post("/api/auth", {
+				username,
+				password,
+			});
+			localStorage.setItem("token", response.data.token);
+			dispatch({ type: "auth_success", payload: response.data.user });
+		} catch (err) {
+			dispatch({ type: "auth_error", payload: err.message });
+		}
+	};
+
+const logout = (dispatch) => () => {
+	localStorage.removeItem("token");
+	dispatch({ type: "logout" });
+};
+
+export const { Context, Provider } = createDataContext(
+	authReducer,
+	{ authenticate, logout },
+	initialState
+);
