3fae6028868c566aa657230c702c64af6759df85
subhasis020299 <subhasisdas125@gmail.com>
2021-11-27 03:02:06 +0530

    added checkusername api route

---
+++ pages/api/users/checkusername.js
@@ -0,0 +1,18 @@
+import { getUser } from "../../../services/user.service";
+import errorHandler from "../../../utils/errorHandler";
+
+export default async function handler(req, res) {
+	if (req.method === "POST") {
+		try {
+			const query = { username: req.body.username };
+			const projection = ["_id", "username"];
+			const user = await getUser(query, projection);
+
+			return res.status(200).json({ available: !user });
+		} catch (error) {
+			errorHandler(error, req, res);
+		}
+	} else {
+		return res.status(405).json({ message: "Method not allowed" });
+	}
+}
